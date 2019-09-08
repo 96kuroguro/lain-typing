d6c83e3aa044f729bad2e251ae69663a8b757baa
96kuroguro <96kuroguro@gmail.com>
2019-09-08 22:56:46 +0900

    dist ignore 解除

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,7 @@ const routerBase = process.env.DEPLOY_ENV === 'GH_PAGES' ? {
 
 export default {
   ...routerBase,
-  
+
   mode: 'spa',
 
   router: {
@@ -24,7 +24,7 @@ export default {
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/lain-typing/favicon.ico' }
     ]
   },
   /*
